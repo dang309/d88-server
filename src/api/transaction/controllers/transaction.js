8e56a435356838774d69dc90d01dd2afa8a12c30
dangn309 <hada.nguyen309@gmail.com>
2024-05-28 13:57:40 +0700

    clean code

--- src/api/transaction/controllers/transaction.js
+++ src/api/transaction/controllers/transaction.js
@@ -1,9 +1,32 @@
-'use strict';
+"use strict";
 
 /**
  * transaction controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::transaction.transaction');
+module.exports = createCoreController(
+  "api::transaction.transaction",
+  ({ strapi }) => {
+    return {
+      async create(ctx) {
+        const user = ctx.state.user
+        const balance = parseInt(user?.balance, 10) || 0;
+        const betAmount = parseInt(ctx.request.body?.data?.betAmount, 10) || 0;
+
+        if (betAmount > balance) return ctx.badRequest("Không đủ chip")
+
+        const newBalance = balance - betAmount;
+
+        const response = await super.create(ctx);
+
+        await strapi.services['plugin::users-permissions.user'].edit(user.id, {
+            balance: newBalance
+        })
+
+        return response;
+      },
+    };
+  }
+);
